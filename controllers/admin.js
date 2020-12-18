b5743f37a68541c0da0ffcd3ab211b48cb4a87a5
Starlight Romero <starlightromero@gmail.com>
2020-12-18 14:32:17 -0800

    [update] getEditProduct, postEditProduct, postDeleteProduct

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,6 +18,38 @@ exports.postAddProduct = (req, res) => {
   res.redirect('/')
 }
 
+exports.getEditProduct = (req, res) => {
+  const productId = req.params.productId
+  Product.findById(productId, product => {
+    if (!product) {
+      return res.redirect('/')
+    }
+    res.render('admin/edit-product', {
+      title: 'Edit Product',
+      product
+    })
+  })
+}
+
+exports.postEditProduct = (req, res) => {
+  const { productId, title, imageUrl, price, description } = req.body
+  const product = new Product(
+    title,
+    imageUrl,
+    price,
+    description,
+    productId
+  )
+  product.save()
+  res.redirect('/admin/products')
+}
+
+exports.postDeleteProduct = (req, res) => {
+  const productId = req.body.productId
+  Product.deleteById(productId)
+  res.redirect('/admin/products')
+}
+
 exports.getProducts = (req, res) => {
   Product.fetchAll(products => {
     res.render('admin/product-list', {
