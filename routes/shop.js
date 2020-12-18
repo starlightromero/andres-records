266e37319a3c6f5bea8becf1acfffe7f09342ac9
Starlight Romero <starlightromero@gmail.com>
2020-12-17 18:18:44 -0800

    [update] shop inputs

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const productsController = require('../controllers/products')
+const shopController = require('../controllers/shop')
 
 const shop = express.Router()
 
-shop.get('/', productsController.getProducts)
+shop.get('/', shopController.getIndex)
+shop.get('/products', shopController.getProducts)
+shop.get('/cart', shopController.getCart)
+shop.get('/checkout', shopController.getCheckout)
 
 module.exports = shop
