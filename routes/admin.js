9d616ce818fdd8bd6c647c74a7a6bb2647b5a369
Starlight Romero <starlightromero@gmail.com>
2020-12-18 14:31:04 -0800

    [update] admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,9 @@ const admin = express.Router()
 
 admin.get('/add-product', adminController.getAddProduct)
 admin.post('/add-product', adminController.postAddProduct)
-// admin.get('/edit-product/:id', adminController.getEditProduct)
-// admin.post('/edit-product/:id', adminController.postEditProduct)
+admin.get('/edit-product/:productId', adminController.getEditProduct)
+admin.post('/edit-product', adminController.postEditProduct)
+admin.post('/delete-product', adminController.postDeleteProduct)
 admin.get('/products', adminController.getProducts)
 
 module.exports = admin
