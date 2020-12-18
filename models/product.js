23b8c32ab4c31a87844bb20b4574b365309fb6ff
Starlight Romero <starlightromero@gmail.com>
2020-12-18 09:35:02 -0800

    [update] product model

--- models/product.js
+++ models/product.js
@@ -16,6 +16,7 @@ const getProductsFromFile = cb => {
 
 module.exports = class Product {
   constructor (title, imageUrl, price, description) {
+    this.id = Math.random().toString()
     this.title = title
     this.imageUrl = imageUrl
     this.price = price
@@ -28,6 +29,16 @@ module.exports = class Product {
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err)
       })
+    })
+  }
+
+  static findById (id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id)
+      console.log('')
+      console.log(product)
+      console.log('')
+      cb(product)
     })
   }
 
