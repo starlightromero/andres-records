8382a659a9a2cbb4b661c18444ea05ec97223380
Starlight Romero <starlightromero@gmail.com>
2020-12-18 15:00:52 -0800

    [update] getCart and postCartDeleteProduct

--- controllers/shop.js
+++ controllers/shop.js
@@ -27,7 +27,20 @@ exports.getProduct = (req, res) => {
 }
 
 exports.getCart = (req, res) => {
-  res.render('shop/cart')
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = []
+      for (const product of products) {
+        const cartProductData = cart.products.find(prod => prod.id === product.id)
+        if (cartProductData) {
+          cartProducts.push({ product: product, qty: cartProductData.qty })
+        }
+      }
+      res.render('shop/cart', {
+        products: cartProducts
+      })
+    })
+  })
 }
 
 exports.postCart = (req, res) => {
@@ -36,6 +49,14 @@ exports.postCart = (req, res) => {
     Cart.addProduct(productId, product.price)
   })
   res.redirect('/cart')
+}
+
+exports.postCartDeleteProduct = (req, res) => {
+  const productId = req.body.productId
+  Product.findById(productId, product => {
+    Cart.deleteProduct(productId, product.price)
+    res.redirect('/cart')
+  })
 }
 
 exports.getOrders = (req, res) => {
