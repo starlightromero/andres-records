9ba232b8e10b141c945835c6c47b099af389a524
Starlight Romero <starlightromero@gmail.com>
2020-12-18 08:53:09 -0800

    [update] product model

--- models/product.js
+++ models/product.js
@@ -15,11 +15,11 @@ const getProductsFromFile = cb => {
 }
 
 module.exports = class Product {
-  constructor (title, imageUrl, description, price) {
+  constructor (title, imageUrl, price, description) {
     this.title = title
     this.imageUrl = imageUrl
-    this.description = description
     this.price = price
+    this.description = description
   }
 
   save () {
