0990a730db98243621c9e920704698ec0cdeef00
Starlight Romero <starlightromero@gmail.com>
2020-12-18 09:35:46 -0800

    [update] shop controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -18,8 +18,23 @@ exports.getProducts = (req, res) => {
   })
 }
 
+exports.getProduct = (req, res) => {
+  const productId = req.params.productId
+  console.log('')
+  console.log(productId)
+  console.log('')
+  Product.findById(productId, product => {
+    res.render('shop/product-detail', { product: product })
+  })
+}
+
 exports.getCart = (req, res) => {
   res.render('shop/cart')
+}
+
+exports.postCart = (req, res) => {
+  const productId = req.body.productId
+  res.redirect('/cart')
 }
 
 exports.getOrders = (req, res) => {
